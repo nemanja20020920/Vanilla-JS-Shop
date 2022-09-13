e7caf81e75c3188fb6c2ece41fd1bbb03f5354aa
Nemanja Djakovic <nemanjadjakovic12378@gmail.com>
2022-09-14 01:45:42 +0200

    Added the Session class.(Task #12)

---
+++ js/Session.js
@@ -0,0 +1,33 @@
+class Session {
+  userId = null;
+
+  startSession() {
+    let date = new Date();
+
+    date.setTime(date.getTime() + 5000);
+
+    let expires = ";expires=" + date.toUTCString();
+
+    document.cookie = "user_id=" + this.userId + expires;
+
+    // 1 * 24 * 60 * 60 * 1000
+  }
+
+  getSession() {
+    let userId = "user_id=";
+    let cookie = document.cookie.split(";");
+
+    for (let part = 0; part < cookie.length; part++) {
+      while (cookie[part] == " ") {
+        cookie[part] = cookie[part].substring(1);
+      }
+
+      if (cookie[part].indexOf(userId) == 0) {
+        console.log(cookie[part].substring(userId.length, cookie[part].length));
+        return cookie[part].substring(userId.length, cookie[part].length);
+      }
+    }
+
+    return "";
+  }
+}
